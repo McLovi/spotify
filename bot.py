bdd5287c4cf378045ad64bfb3ac464a0681c0353
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-08-01 10:00:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
@@ -5,31 +6,30 @@ import random
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix="$")
-lines = open(r'usermcalts.txt').read().splitlines()
+lines = open(r'accounts.txt').read().splitlines()
 
 @bot.event
 async def on_ready():
-    print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type $spotify'))
-   
-@commands.cooldown(1, 10, commands.BucketType.user)
+    print("The bot is online!")
+
+@commands.cooldown(1, 30, commands.BucketType.user)
 @bot.command(pass_context=True)
 async def spotify(ctx):
     userName = ctx.message.author.name
     userID = ctx.message.author.id
 
     if ctx.message.server:
-        myline = random.choice(lines)
-        split = myline.partition(":")
-       
-        embed=discord.Embed(title="Spotify Account", color=0xf45eff)
-        embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
-        embed.add_field(name="UserName:", value=split[0], inline=False)
-        embed.add_field(name="Password:", value=split[2], inline=False)
-        await bot.send_message(ctx.message.author, embed=embed)
+        await bot.delete_message(ctx.message)
+    myline = random.choice(lines)
+    split = myline.partition(":")
+    
+    embed=discord.Embed(title="Minecraft Account", color=0xf45eff)
+    embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
+    embed.add_field(name="Email:", value=split[0], inline=False)
+    embed.add_field(name="Password:", value=split[2], inline=False)
+    await bot.send_message(ctx.message.author, embed=embed)
 
-        print("{} Typed $minecrafr".format(userName))
-
-        client = discord.Client()
+    print("{} Typed !gen".format(userName))
 
 bot.run("NDcyMzQyOTIzMzU5NDIwNDE2.DkLqCg.hZ3XrkM3CVDi2gNwVEBOXsjMQp8")
+
