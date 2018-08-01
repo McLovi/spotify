8a2d21065b2fb72a15e066a1f0d5474311bed97b
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-08-01 09:50:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,14 +5,14 @@ import random
 
 Client = discord.Client()
 bot = commands.Bot(command_prefix="$")
-lines = open(r'spotify.txt').read().splitlines()
+lines = open(r'usermcalts.txt').read().splitlines()
 
 @bot.event
 async def on_ready():
     print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type $spotify'))
+    await bot.change_presence(game=discord.Game(name='Type $minecraft'))
    
-    
+@commands.cooldown(1, 10, commands.BucketType.user)
 @bot.command(pass_context=True)
 async def spotify(ctx):
     userName = ctx.message.author.name
@@ -23,12 +23,12 @@ async def spotify(ctx):
         split = myline.partition(":")
        
         embed=discord.Embed(title="Spotify Account", color=0xf45eff)
-        embed.set_thumbnail(url="https://images.complex.com/complex/images/c_limit,w_680/fl_lossy,pg_1,q_auto/ko5zhcsynomwhiqqykbr/spotify-headphones")
-        embed.add_field(name="Email:", value=split[0], inline=False)
+        embed.set_thumbnail(url="https://pre00.deviantart.net/3d13/th/pre/i/2016/343/0/7/free_minecraft_logo_template_by_curtzeninja-dar2dou.png")
+        embed.add_field(name="UserName:", value=split[0], inline=False)
         embed.add_field(name="Password:", value=split[2], inline=False)
         await bot.send_message(ctx.message.author, embed=embed)
 
-        print("{} Typed $spotify".format(userName))
+        print("{} Typed $minecrafr".format(userName))
 
         client = discord.Client()
 
