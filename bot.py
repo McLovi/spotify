3aea5640a7b4c34d1efcf90c0916e485f6cb9b83
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-08-01 08:53:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,4 +32,4 @@ async def spotify(ctx):
 
         client = discord.Client()
 
-bot.run("NDcyMzQyOTIzMzU5NDIwNDE2.Dj3LhA.Psy5URk_eLiJzz5qsrmp-N5KncA")
+bot.run("NDcyMzQyOTIzMzU5NDIwNDE2.DkLqCg.hZ3XrkM3CVDi2gNwVEBOXsjMQp8")
