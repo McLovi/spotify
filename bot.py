267b5f52de287ad7d038233817c9ef3f15f30e1b
McLovi <41690172+McLovi@users.noreply.github.com>
2018-08-02 23:38:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ lines = open(r'spotify.txt').read().splitlines()
 @bot.event
 async def on_ready():
     print('The bot is online!')
-    await bot.change_presence(game=discord.Game(name='Type $uplay'))
+    await bot.change_presence(game=discord.Game(name='Type $spotify'))
    
 @commands.cooldown(1, 15, commands.BucketType.user)
 @bot.command(pass_context=True)
